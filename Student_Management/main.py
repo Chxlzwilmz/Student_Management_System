5e1babb05b1392b4df560a5644e744d49583d0ea
Charles A <143869950+Chxlzwilmz@users.noreply.github.com>
2025-08-31 12:13:26 +0530

    main.py

--- Student_Management/main.py
+++ Student_Management/main.py
@@ -3,15 +3,15 @@ import mysql.connector
 # Connect to MySQL database
 conn = mysql.connector.connect(
     host="localhost",
-    user="MySQL",  # Change this to your MySQL username
-    password="MySQL",  # Change this to your MySQL password
+    user="root", 
+    password="MySQL", 
     database="std_db"
 )
 cursor = conn.cursor()
 
 # Function to add a student
 def add_student(name, age, course):
-    sql = "INSERT INTO students (name, age, course) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO students (name, age, course) VALUES ("Charles", 20,"BCA")"
     values = (name, age, course)
     cursor.execute(sql, values)
     conn.commit()
@@ -26,7 +26,7 @@ def view_students():
 
 # Function to update a student
 def update_student(student_id, name, age, course):
-    sql = "UPDATE students SET name = %s, age = %s, course = %s WHERE id = %s"
+    sql = "UPDATE students SET name = "Karthik", age = 21, course = "IT" WHERE id = 7"
     values = (name, age, course, student_id)
     cursor.execute(sql, values)
     conn.commit()
@@ -39,7 +39,7 @@ def delete_student(student_id):
     conn.commit()
     print("Student deleted successfully!")
 
-# Menu-driven program
+# for menu program
 while True:
     print("\nStudent Management System")
     print("1. Add Student")
